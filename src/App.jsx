41fa3b2fe0a9eec3dca95d671153083d677c8bfc
CatInPajamas <gaurabkhatry@gmail.com>
2022-09-11 13:56:55 +0545

    added settings

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,23 @@ import Timer from "./Timer"
 
 function App() {
 
-  const [isSettings, setIsSettings] = useState(false);
+  const [isSettings, setIsSettings] = useState(true);
   const [workTime, setWorkTime] = useState(1);
   const [paused, setPause] = useState(false);
   const [breakTime, setBreakTime] = useState(1);
+  
+  let flipSettings = () => {
+    setIsSettings(!isSettings);
+  }
+
   return (
     <div id="App">
-      {isSettings ? <Settings setIsSettings={setIsSettings} paused={paused} setPause={setPause}/> : <Timer workTime={workTime} breakTime={breakTime} setIsSettings={setIsSettings} />}
+      {isSettings ? <Settings workTime={workTime} setWorkTime={setWorkTime} breakTime={breakTime} setBreakTime={setBreakTime} isSettings={isSettings} setIsSettings={setIsSettings} /> : <Timer workTime={workTime} breakTime={breakTime} setIsSettings={setIsSettings} />}
+      <button onClick={flipSettings}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+          <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
+        </svg>
+      </button>
     </div>
   )
 }
--- src/Settings.jsx
+++ src/Settings.jsx
@@ -1,4 +1,19 @@
-function Settings() {
-  
+import ReactSlider from "react-slider";
+import "./Settings.css"
+export default function Settings({workTime, setWorkTime, breakTime, setBreakTime, isSettings, isSetSettings}) {
+    let workTimeChange = (val) => {
+        setWorkTime(val);
+    }
+    let breakTimeChange = (val) => {
+        setBreakTime(val);
+    }
+    return (
+        <>
+        <label>Work Time : {workTime} </label>
+        <ReactSlider className={"slider"} thumbClassName={"thumb"} trackClassName={"track"} value={workTime} max={120} min={5} onChange={workTimeChange}/>
+        <br/>
+        <label>Break Time : {breakTime} </label>
+        <ReactSlider className={"slider break"} thumbClassName={"thumb break"} trackClassName={"track"} value={breakTime} max={120} min={5} onChange={breakTimeChange}/>
+        </>
+    )
 }
-export default Settings;
